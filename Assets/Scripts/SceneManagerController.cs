861ebb2fdcc05e3d490ca471c76ccb7a23e9ba26
Tutita-Dev <mauro.tupac@gmail.com>
2023-10-08 06:25:36 -0300

    Todo Funcional

--- Assets/Scripts/SceneManagerController.cs
+++ Assets/Scripts/SceneManagerController.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using TMPro;
+
 
 public class SceneManagerController : MonoBehaviour
 {
     public GameObject[] allMenues;
     public GameObject wantedActiveMenu;
 
-    // variables para el temporizador
-    public float tiempoTranscurrido = 0f;
-    public TextMeshProUGUI textoTiempo;
-    public float duracionMaxima = 10f;
+
 
     private void Start()
     {
@@ -27,19 +24,7 @@ public class SceneManagerController : MonoBehaviour
         if (wantedActiveMenu != null) wantedActiveMenu.SetActive(true);
     }
 
-    private void Update()
-    {
-        tiempoTranscurrido += Time.deltaTime;
-
-        if(tiempoTranscurrido >= duracionMaxima)
-        {
-            SceneManager.LoadScene("SaveScore");
-        }
-
-        int minutos = Mathf.FloorToInt(tiempoTranscurrido / 60);
-        int segundos = Mathf.FloorToInt(tiempoTranscurrido % 60);
-        textoTiempo.text = string.Format("{0:00}:{1:00}", minutos, segundos);
-    }
+  
 
     public void ChangeScene(Object scene)
     {
