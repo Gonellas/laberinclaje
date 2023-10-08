e2152e2153eee155b0a7314d9bc99aa27e7684bc
Camila Gonella <camilamgonella@gmail.com>
2023-10-08 18:17:48 -0300

    fix finales

--- Assets/Scripts/Contaminacion.cs
+++ Assets/Scripts/Contaminacion.cs
@@ -8,7 +8,7 @@ public class Contaminacion : MonoBehaviour
 {
     public float alphaActual = 0.0f;
     public float alphaMaximo = 1.0f;
-    public float velocidadAumentoAlpha = 0.01f;
+    public float velocidadAumentoAlpha = 0.005f;
 
     private Image imageContaminacion;
 
